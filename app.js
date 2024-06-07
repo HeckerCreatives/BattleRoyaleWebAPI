2a40fea2f14ff2beec632ef03d67ec37f807702f
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-06-07 15:32:05 +0800

    posshyyy

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const app = express();
 
 const {initialize} = require("./initialization/serverinitialize")
 
+const CORS_ALLOWED = process.env.ALLOWED_CORS
+
 const corsConfig = {
-    origin: ["http://localhost:5173/","http://localhost:5173", "http://localhost:3000/", "http://localhost:3000", "https://battleroyalelandingpage.onrender.com", "https://battleroyalelandingpage.onrender.com/", "https://battleroyalewebsite.onrender.com", "https://battleroyalewebsite.onrender.com/"],
+    origin: CORS_ALLOWED.split(" "),
     methods: ["GET", "POST", "PUT", "DELETE"], // List only` available methods
     credentials: true, // Must be set to true
     allowedHeaders: ["Origin", "Content-Type", "X-Requested-With", "Accept", "Authorization"],
