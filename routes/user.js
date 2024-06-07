c87cb64f230ac722d5c3618d2fcc8b607456c7f4
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-06-07 16:54:41 +0800

    possssh

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { getuserdetails, changepassworduser, getplayerlist, updateuserprofile, banunbanuser, getplayercount, getplayersbystatus } = require("../controllers/user")
+const { getuserdetails, changepassworduser, getplayerlist, updateuserprofile, banunbanuser, getplayercount, getplayersbystatus, getregistrationcount, getuserregistrationchart } = require("../controllers/user")
 const { protectplayer, protectsuperadmin } = require("../middleware/middleware")
 
 router
@@ -10,5 +10,7 @@ router
     .post("/banunbanuser", protectsuperadmin, banunbanuser)
     .get("/getplayercount", protectsuperadmin, getplayercount)
     .get("/getplayerbystatus", protectsuperadmin, getplayersbystatus)
+    .get("/getregistrationcount", protectsuperadmin, getregistrationcount)
+    .get("/getuserregistrationchart", protectsuperadmin, getuserregistrationchart)
     
 module.exports = router;
