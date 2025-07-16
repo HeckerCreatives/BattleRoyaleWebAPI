8969c286f54d19e17ae4c9045325320df478a0bc
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-07-16 19:13:46 +0800

    Update season.js

--- routes/season.js
+++ routes/season.js
@@ -13,8 +13,8 @@ const { protectplayer, protectsuperadmin, protectalluser } = require("../middlew
 
 router
     .post("/createseason", protectsuperadmin, createseason)
-    .get("/getallseasons", protectplayer, getallseasons)
-    .get("/getcurrentseason", protectplayer, getcurrentseason)
+    .get("/getallseasons", protectalluser, getallseasons)
+    .get("/getcurrentseason", protectalluser, getcurrentseason)
     .get("/getseasonsuperadmin", protectsuperadmin, getseasonsuperadmin)
     .post("/startseason", protectsuperadmin, startseason)
     .post("/endseason", protectsuperadmin, endseason)
