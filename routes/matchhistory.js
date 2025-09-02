c64bf1d569413f237bb8b34d6b597fbc1ba0f4c3
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-09-02 18:42:10 +0800

    Update matchhistory.js

--- routes/matchhistory.js
+++ routes/matchhistory.js
@@ -1,10 +1,10 @@
 const { getplayermatchhistory, getmatchhistory } = require("../controllers/matchhistory")
-const { protectsuperadmin } = require("../middleware/middleware")
+const { protectsuperadmin, protectplayer } = require("../middleware/middleware")
 
 const router = require("express").Router()
 
 router
  .get("/admin/viewmatchhistory", protectsuperadmin, getplayermatchhistory)
- .get("/viewmatchhistory", protectsuperadmin, getmatchhistory)
+ .get("/viewmatchhistory", protectplayer, getmatchhistory)
 
 module.exports = router
